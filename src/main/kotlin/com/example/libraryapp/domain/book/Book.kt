97dec0ea56b985f97c4a67477d71e03a4e2d6952
hanna1122 <khn951410@naver.com>
2023-12-08 22:45:17 +0900

    feat: Book 도메인 추가

---
+++ src/main/kotlin/com/example/libraryapp/domain/book/Book.kt
@@ -0,0 +1,34 @@
+package com.group.libraryapp.domain.book
+
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+
+class Book(
+  val name: String,
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  val id: Long? = null
+) {
+
+  init {
+    if (name.isBlank()) {
+      throw IllegalArgumentException("이름은 비어 있을 수 없습니다")
+    }
+  }
+
+  companion object {
+    fun fixture(
+      name: String = "책 이름",
+      type: BookType = BookType.COMPUTER,
+      id: Long? = null,
+    ): Book {
+      return Book(
+        name = name,
+        id = id,
+      )
+    }
+  }
+
+}
