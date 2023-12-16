0d5669276e206a2e6b8f3bbee60b147254f5538d
hanna1122 <khn951410@naver.com>
2023-12-16 23:49:47 +0900

    feat: add book loaning and returning capabilities

--- src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
@@ -2,4 +2,6 @@ package com.group.libraryapp.domain.user.loanhistory
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserLoanHistoryRepository : JpaRepository<UserLoanHistory, Long>
+interface UserLoanHistoryRepository : JpaRepository<UserLoanHistory, Long> {
+    fun findByBookNameAndIsReturn(bookName: String?, isReturn: Boolean): UserLoanHistory?
+}
