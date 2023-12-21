fc28089baf46dbeebffdb8e4459e1ef953dfb738
hanna1122 <khn951410@naver.com>
2023-12-21 23:32:04 +0900

    feat: add saveUser, getUsers

---
+++ src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -0,0 +1,27 @@
+package com.group.libraryapp.service.user
+
+import com.group.libraryapp.domain.user.User
+import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.response.UserResponse
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class UserService(
+  private val userRepository: UserRepository,
+) {
+
+  @Transactional
+  fun saveUser(request: UserCreateRequest) {
+    val newUser = User(request.name, request.age)
+    userRepository.save(newUser)
+  }
+
+  @Transactional(readOnly = true)
+  fun getUsers(): List<UserResponse> {
+    return userRepository.findAll()
+      .map { user -> UserResponse(user) }
+  }
+
+}
