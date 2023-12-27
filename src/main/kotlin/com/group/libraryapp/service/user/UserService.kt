e976cd3fd78213c8ca240042e8e10ddee70cb057
hanna1122 <khn951410@naver.com>
2023-12-27 22:29:38 +0900

    feat: add updateUserName, deleteUser to UserService

--- src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
+++ src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -3,6 +3,7 @@ package com.group.libraryapp.service.user
 import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.request.UserUpdateRequest
 import com.group.libraryapp.dto.user.response.UserResponse
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -22,6 +23,18 @@ class UserService(
   fun getUsers(): List<UserResponse> {
     return userRepository.findAll()
       .map { user -> UserResponse(user) }
+  }
+
+  @Transactional
+  fun updateUserName(request: UserUpdateRequest) {
+    val user = userRepository.findById(request.id).orElseThrow(::IllegalArgumentException)
+    user.updateName(request.name)
+  }
+
+  @Transactional
+  fun deleteUser(name: String) {
+    val user = userRepository.findByName(name)?: throw IllegalArgumentException("not found book")
+    userRepository.delete(user)
   }
 
 }
