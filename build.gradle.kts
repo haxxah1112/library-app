87c08623408ef0ac7e3138f65ab5b386e805638b
hanna1122 <khn951410@naver.com>
2024-01-01 21:12:10 +0900

    chore: add kotest dependency to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,6 +50,10 @@ dependencies {
 	runtimeOnly("com.h2database:h2")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.mockk:mockk:1.13.4")
 }
 
 kapt {
