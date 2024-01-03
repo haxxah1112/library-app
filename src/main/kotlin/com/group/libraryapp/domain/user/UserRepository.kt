4acb050696bd4ad65946825f65fc4cede11f8b94
hanna1122 <khn951410@naver.com>
2024-01-03 23:46:28 +0900

    feat: add findAllWithHistories using JPQL

--- src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
@@ -1,11 +1,14 @@
 package com.group.libraryapp.domain.user
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UserRepository : JpaRepository<User, Long>, UserRepositoryCustom {
+interface UserRepository : JpaRepository<User, Long> {
 
   fun findByName(name: String): User?
 
+  @Query("SELECT DISTINCT u FROM User u LEFT JOIN FETCH u.userLoanHistories")
+  fun findAllWithHistories(): List<User>
 }
