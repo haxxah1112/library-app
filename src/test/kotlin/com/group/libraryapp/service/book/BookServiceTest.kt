c85d3fbdeaf5f79db8f025e3aafef24f124eea70
hanna1122 <khn951410@naver.com>
2024-01-19 21:12:48 +0900

    test: add getBookStatisticsTest

--- src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
@@ -11,6 +11,7 @@ import com.group.libraryapp.domain.user.loanhistory.UserLoanStatus
 import com.group.libraryapp.dto.book.request.BookLoanRequest
 import com.group.libraryapp.dto.book.request.BookRequest
 import com.group.libraryapp.dto.book.request.BookReturnRequest
+import com.group.libraryapp.dto.book.response.BookStatResponse
 import com.group.libraryapp.service.book.BookService
 import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
 import org.junit.jupiter.api.AfterEach
@@ -122,6 +123,28 @@ class BookServiceTest (
 
     //then
     assertThat(result).isEqualTo(1)
+  }
+
+  @Test
+  @DisplayName("분야별 책 권수를 정상 확인한다")
+  fun getBookStatisticsTest() {
+    // given
+    bookRepository.saveAll(listOf(
+      Book.fixture("A", BookType.COMPUTER),
+      Book.fixture("B", BookType.COMPUTER),
+      Book.fixture("C", BookType.SCIENCE),
+    ))
+
+    // when
+    val results = bookService.getBookStatistics()
+
+    // then
+    assertThat(results).hasSize(2)
+    val computerDto = results.first { result -> result.type == BookType.COMPUTER }
+    assertThat(computerDto.count).isEqualTo(2)
+
+    val scienceDto = results.first { result -> result.type == BookType.SCIENCE }
+    assertThat(scienceDto.count).isEqualTo(1)
   }
 
 }
