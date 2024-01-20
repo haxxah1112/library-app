bac0a32a42321e8587ca270b0559fc8e69c0f814
hanna1122 <khn951410@naver.com>
2024-01-21 00:04:15 +0900

    refactor: refactored getBookStatistics function.

--- src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
+++ src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
@@ -48,16 +48,8 @@ class BookService(
   }
 
   fun getBookStatistics(): List<BookStatResponse> {
-    val results = mutableListOf<BookStatResponse>()
-    val books = bookRepository.findAll()
-    for (book in books) {
-      val targetDto = results.firstOrNull { dto -> book.type == dto.type }
-      if (targetDto == null) {
-        results.add(BookStatResponse(book.type, 1))
-      } else {
-        targetDto.plusOne()
-      }
-    }
-    return results
+    return bookRepository.findAll()
+      .groupBy { book -> book.type }
+      .map { (type, books) -> BookStatResponse(type, books.size) }
   }
 }
